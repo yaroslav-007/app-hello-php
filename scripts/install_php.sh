ee71bebf5dc1c7a1e27abdb054b152108787f057
Yaroslav <yorimark@abv.bg>
2019-08-12 15:11:02 +0300

    Travis test

--- scripts/install_php.sh
+++ scripts/install_php.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get -y install nginx php php-fpm
+apt-get -y install nginx php php-fpm curl
 [ -f /etc/nginx/sites-available/default ] && unlink /etc/nginx/sites-available/default
 cp /vagrant/conf/nginx.conf /etc/nginx/conf.d/php.conf
 
