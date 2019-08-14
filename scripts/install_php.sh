3a5f0bf289bf7b64de5f8ba4ffcb0c988aa6c962
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2019-08-14 10:12:02 +0200

    Update install_php.sh

--- scripts/install_php.sh
+++ scripts/install_php.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
+set -x
 
+# install nginx and php
 apt-get update
 apt-get -y install nginx php php-fpm curl
 
+systemctl stop nginx
+
+# remove default config
+[ -f /etc/nginx/sites-available/default ] && unlink /etc/nginx/sites-available/default
+
+# change dir to /vagrant if we are in a vagrant vm
 [ -d /vagrant ] && pushd /vagrant
 
-[ -f /etc/nginx/sites-available/default ] && unlink /etc/nginx/sites-available/default
+# deploy our conf file
 cp conf/nginx.conf /etc/nginx/conf.d/php.conf
 
-systemctl restart nginx
+systemctl start nginx
